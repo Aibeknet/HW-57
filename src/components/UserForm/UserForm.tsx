cb30b08d7d2f93e479091ceda163d79db034569b
aibeknet <aaskarov0690.kg@gmail.com>
2024-10-03 16:38:15 +0600

    The UserForm component was created.As well as the file type.ts with a User interface.

---
+++ src/components/UserForm/UserForm.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import { User} from '../../type.ts';
+
+const UserForm = ({addUser}: {addUser:(user: User) => void}) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [active, setActive] = useState(false);
+  const [role, setRole] = useState('user');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    addUser({name, email, active, role});
+    setName('');
+    setEmail('');
+    setActive(false);
+    setRole('user');
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+          className="form-control"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="email" className="form-label">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          className="form-control"
+        />
+      </div>
+
+      <div className="mb-3 form-check">
+        <input
+          type="checkbox"
+          id="active"
+          checked={active}
+          onChange={(e) => setActive(e.target.checked)}
+          required
+          className="form-check-input"
+        />
+        <label htmlFor="active" className="form-check-label">Active</label>
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="role" className="form-label">Role</label>
+        <select
+          id="role"
+          value={role}
+          onChange={(e) => setRole(e.target.value)}
+          className="form-select"
+        >
+          <option value="user">User</option>
+          <option value="editor">Editor</option>
+          <option value="admin">Admin</option>
+        </select>
+      </div>
+
+      <button type="submit" className="btn btn-primary">Add User</button>
+    </form>
+  );
+};
+
+export default UserForm;
