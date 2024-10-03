b90955d62f67493919e5580767ade529cefaf702
aibeknet <aaskarov0690.kg@gmail.com>
2024-10-03 16:52:02 +0600

    The UserItem component was created. To output a single user.

---
+++ src/components/UserItem/UserItem.tsx
@@ -0,0 +1,15 @@
+import { User} from '../../type';
+
+interface UserItemProps {
+  user: User;
+}
+
+const UserItem: React.FC<UserItemProps> = ({user}) => {
+  return (
+    <li className="list-group-item">
+      <strong>{user.name}</strong> - {user.email} - {user.role} - {user.active ? 'Active' : 'Inactive'}
+    </li>
+  );
+};
+
+export default UserItem;
