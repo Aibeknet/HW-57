cc095fea3b32dfcca4a04c64383a58108f0781e8
aibeknet <aaskarov0690.kg@gmail.com>
2024-10-03 17:29:38 +0600

    The UserItem component was created.

---
+++ src/components/UsersList/UsersList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import UserItem from '../UserItem/UserItem.tsx';
+import { User } from '../../type';
+
+interface UsersListProps {
+  users: User[];
+}
+const UsersList: React.FC<UsersListProps> = ({users}) => {
+  return (
+    <ul className="list-group">
+      {users.map((user) => (
+        <UserItem key={user.email} user={user} />
+      ))}
+    </ul>
+  );
+};
+
+export default UsersList;
